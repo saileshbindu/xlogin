be12351a22d4b71148ec51421d1b549914301f9d
saileshbindu <sailesh.bindu189@gmail.com>
2024-06-29 21:13:42 +0530

    xloginpush

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [showMessage, setMessage] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
+
+  const loginDetail = (event) =>{
+    event.preventDefault()
+    if(user === "user" && password == "password"){
+      setMessage(`Welcome ${user}`)
+      setIsLogin(true)
+      setUser('');
+      setPassword('');
+    }
+    else{
+      setMessage("Invalid username and password");
+      setUser('');
+      setPassword('');
+    }
+    
+    
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      <p>{showMessage}</p>
+      {isLogin ? (<div></div>) : ( <form onSubmit={loginDetail} >
+        <div>
+          <label>Username: </label>
+          <input type='text' placeholder='Username' required onChange={(e)=>{setUser(e.target.value)}} value={user}/>
+        </div>
+        <div>
+          <label>Password: </label>
+          <input type='password' placeholder='Password' onChange={(e)=>{setPassword(e.target.value)}} required value={password}/>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>)}
+     
     </div>
   );
 }
